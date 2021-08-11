e4454c35c0e9faff6abacbacf36e725603c4ef6e
Andrea Piseri <andrea.piseri@gmail.com>
2021-08-11 12:06:36 +0200

    parametrized delimiters and separators

---
+++ args.h
@@ -0,0 +1,225 @@
+#ifndef ARGS_H_
+#define ARGS_H_
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void arg_int(const char *name, int *value, const char *def);
+static void arg_float(const char *name, float *value, const char *def);
+static void arg_string(const char *name, char **value, const char *def);
+
+static int arg_parse(int argc, char **argv);
+static void arg_print_usage(FILE *stream);
+
+#endif // ARGS_H_
+
+///////////////////////////
+
+#ifndef ARGS_IMPLEMENTATION
+#define ARGS_IMPLEMENTATION
+
+#ifndef ARGS_COUNT
+#define ARGS_COUNT 256
+#endif
+
+enum arg_h_type {
+  ARG_TYPE_UNINIT = 0,
+  ARG_TYPE_INT,
+  ARG_TYPE_FLOAT,
+  ARG_TYPE_STRING
+};
+
+union arg_h_union {
+  int *Int;
+  char **String;
+  float *Float;
+};
+
+typedef struct {
+  enum arg_h_type type;
+  union arg_h_union as;
+  const char *name;
+  const char *def;
+} Arg;
+
+static struct {
+  Arg a[ARGS_COUNT];
+  size_t num;
+} args;
+
+static inline void arg_int(const char *name, int *value, const char *def) {
+  if (args.num + 1 >= ARGS_COUNT) {
+    fprintf(stderr, "Too many arguments were declared\n");
+    exit(0);
+  }
+  args.a[args.num] = (Arg){
+      .type = ARG_TYPE_INT,
+      .as.Int = value,
+      .name = name,
+      .def = def,
+  };
+  args.num++;
+}
+
+static inline void arg_float(const char *name, float *value, const char *def) {
+  if (args.num + 1 >= ARGS_COUNT) {
+    fprintf(stderr, "Too many arguments were declared\n");
+    exit(0);
+  }
+  args.a[args.num] = (Arg){
+      .type = ARG_TYPE_FLOAT,
+      .as.Float = value,
+      .name = name,
+      .def = def,
+  };
+  args.num++;
+}
+
+static inline void arg_string(const char *name, char **value, const char *def) {
+  if (args.num + 1 >= ARGS_COUNT) {
+    fprintf(stderr, "Too many arguments were declared\n");
+    exit(0);
+  }
+  args.a[args.num] = (Arg){
+      .type = ARG_TYPE_STRING,
+      .as.String = value,
+      .name = name,
+      .def = def,
+  };
+  args.num++;
+}
+
+static inline int arg_parse_at(char *source, Arg a) {
+  int tmp_int;
+  float tmp_float;
+  char *endptr = NULL;
+  switch (a.type) {
+  case ARG_TYPE_INT:
+    tmp_int = strtol(source, &endptr, 0);
+    if (*endptr) {
+      fprintf(stderr,
+              "Argument after \"%s\" must be an integer, found "
+              "\"%s\" instead\n",
+              a.name, source);
+      return 1;
+    }
+    *a.as.Int = tmp_int;
+    break;
+
+  case ARG_TYPE_FLOAT:
+    tmp_float = strtof(source, &endptr);
+    if (*endptr) {
+      fprintf(stderr,
+              "Argument after \"%s\" must be a floating point number, found "
+              "\"%s\" instead\n",
+              a.name, source);
+      return 1;
+    }
+    *a.as.Float = tmp_float;
+    break;
+
+  case ARG_TYPE_STRING:
+    *a.as.String = source;
+    break;
+
+  case ARG_TYPE_UNINIT:
+    exit(0);
+  }
+
+  return 0;
+}
+
+static inline int is_eq_arg(const char *name, const char *buf) {
+  int i;
+  for (i = 0; name[i] != '\0'; i++)
+    if (name[i] != buf[i])
+      return 0;
+  return buf[i] == '=' ? 1 : 0;
+}
+
+static inline int arg_parse(int argc, char **argv) {
+
+  enum { parsed, available } state[argc];
+  for (int i = 1; i < argc; i++)
+    state[i] = available;
+  state[0] = parsed;
+
+  for (size_t i = 0; i < args.num; i++) {
+    for (int j = 0; j < argc; j++) {
+      if (state[j] == available && strcmp(args.a[i].name, argv[j]) == 0) {
+        state[j] = parsed;
+        if (args.a[i].type == ARG_TYPE_UNINIT) {
+          fprintf(stderr, "Tried to parse uninitialized argument");
+          return 1;
+        }
+
+        if (j == argc - 1) {
+          fprintf(stderr, "value expected after \"%s\"\n", args.a[i].name);
+          return 1;
+        }
+
+        if (state[j + 1] == parsed) {
+          fprintf(stderr, "Tried to parse the same argument twice (%s)\n",
+                  argv[j + 1]);
+          return 1;
+        }
+        state[j + 1] = parsed;
+        if (arg_parse_at(argv[j + 1], args.a[i]))
+          return 1;
+      } else if (state[j] == available && is_eq_arg(args.a[i].name, argv[j])) {
+        state[j] = parsed;
+        if (args.a[i].type == ARG_TYPE_UNINIT) {
+          fprintf(stderr, "Tried to parse uninitialized argument");
+          return 1;
+        }
+
+        if (arg_parse_at(argv[j] + strlen(args.a[i].name) + 1, args.a[i]))
+          return 1;
+      }
+    }
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (state[i] == available) {
+      fprintf(stderr, "WARNING: unrecognized argument %s\n", argv[i]);
+    }
+  }
+
+  return 0;
+}
+
+static inline void arg_print_usage(FILE *stream) {
+  fprintf(stream, "Usage:\n");
+  for (size_t i = 0; i < args.num; i++) {
+    Arg a = args.a[i];
+    switch (a.type) {
+    case ARG_TYPE_INT:
+      fprintf(stream,
+              "\t%s <int>\n"
+              "\t\t%s\n",
+              a.name, a.def);
+      break;
+    case ARG_TYPE_FLOAT:
+      fprintf(stream,
+              "\t%s <float>\n"
+              "\t\t%s\n",
+              a.name, a.def);
+      break;
+    case ARG_TYPE_STRING:
+      fprintf(stream,
+              "\t%s <string>\n"
+              "\t\t%s\n",
+              a.name, a.def);
+      break;
+
+    case ARG_TYPE_UNINIT:
+      fprintf(stderr, "ERROR: Tried to print usage of uninitialized arg\n");
+      exit(1);
+      break;
+    }
+  }
+}
+
+#endif // ARGS_IMPLEMENTATION
--- tablify.c
+++ tablify.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+#include "args.h"
 #include "stringview.h"
+#include <stdio.h>
 
 char *ignore = "/";
+char *seps = "-=";
+char *delim = "|";
 
 typedef struct {
   size_t lines;
@@ -23,42 +26,45 @@ char separator_line(string_view s);
 void pad(size_t width, char fill, FILE *stream);
 void print_entry(string_view s, size_t width, FILE *stream, align align);
 string_view slurp_stream(FILE *stream);
-geometry get_table_geometry(string_view f, char delim);
+geometry get_table_geometry(string_view f);
 void read_table(geometry g, string_view f, string_view *table, size_t *width,
-                char *separators, char delim);
+                char *separators);
 void print_table(geometry g, string_view *table, size_t *width,
-                 char *separators, char delim, FILE *stream);
+                 char *separators, FILE *stream);
 
-int main() {
+int main(int argc, char **argv) {
 
-  char delim = '|';
+  arg_string("-ignore", &ignore,
+             "The characters to ignore when determining if a line should "
+             "contain a separator.");
+  arg_string("-delim", &delim, "the table delimiter");
+  arg_string("-separators", &seps, "the line separator characters");
+
+  arg_parse(argc, argv);
+
   string_view f = slurp_stream(stdin);
 
   // read the table once to get the dimensions
-  geometry g = get_table_geometry(f, delim);
+  geometry g = get_table_geometry(f);
 
   // allocate a table for the SV's, an array for width info, and one for
   // the separator positions.
-  string_view *table = calloc (
-          g.lines * g.cols * sizeof(string_view) +  // table
-          g.cols * sizeof(size_t) +                 // widths
-          g.lines * sizeof(char),                   // separators
-          1
-          );
-  size_t *width = (size_t*) (table + g.lines * g.cols);
-  char *separators = (char*) (width + g.cols);
+  string_view *table = calloc(g.lines * g.cols * sizeof(string_view) + // table
+                                  g.cols * sizeof(size_t) +            // widths
+                                  g.lines * sizeof(char), // separators
+                              1);
+  size_t *width = (size_t *)(table + g.lines * g.cols);
+  char *separators = (char *)(width + g.cols);
 
-  read_table(g, f, table, width, separators, delim);
+  read_table(g, f, table, width, separators);
 
-  print_table(g, table, width, separators, delim, stdout);
+  print_table(g, table, width, separators, stdout);
 
   free(f.data);
   free(table); // this will also free the other arrays
 }
 
-int good_separator(char c) { 
-	return c == '-' || c == '=';
-}
+int good_separator(char c) { return c == '-' || c == '='; }
 
 int contains(char *s, char c) {
   for (size_t i = 0; s[i] != '\0'; i++)
@@ -68,11 +74,11 @@ int contains(char *s, char c) {
 }
 
 int sv_len_utf_8(string_view s) {
-	int ret = 0;
-	for (;s.len > 0; s.len--)
-	  if (((*s.data++) & 0xc0) != 0x80)
-	    ret++;
-	return ret;
+  int ret = 0;
+  for (; s.len > 0; s.len--)
+    if (((*s.data++) & 0xc0) != 0x80)
+      ret++;
+  return ret;
 }
 
 // return the separator character if the line is supposed to be a
@@ -80,13 +86,13 @@ int sv_len_utf_8(string_view s) {
 char separator_line(string_view s) {
   char sep = '\0';
   for (size_t i = 0; i < s.len; i++)
-    if (!sep && good_separator(s.data[i]))
-	  sep = s.data[i];
-    else if ( isspace(s.data[i]) || good_separator(s.data[i]) || 
-			s.data[i] == '|'   || contains(ignore, s.data[i]) )
-	  continue;
-    else 
-	  return 0;
+    if (!sep && contains(seps, s.data[i]))
+      sep = s.data[i];
+    else if (isspace(s.data[i]) || s.data[i] == sep ||
+             s.data[i] == *delim || contains(ignore, s.data[i]))
+      continue;
+    else
+      return 0;
   return sep;
 }
 
@@ -97,35 +103,33 @@ void pad(size_t width, char fill, FILE *stream) {
 }
 
 void print_entry(string_view s, size_t width, FILE *stream, align align) {
-  size_t pad_left = 0, 
-		 pad_right = 0, 
-		 utf8len = sv_len_utf_8(s);
+  size_t pad_left = 0, pad_right = 0, utf8len = sv_len_utf_8(s);
 
   switch (align) {
-	  case left:
-		  pad_left = 1;
-		  pad_right = width - utf8len -pad_left;
-		  break;
-	  case left_nopad:
-		  pad_left = 0;
-		  pad_right = width - utf8len -pad_left;
-		  break;
-	  case center:
-		  pad_left = (width -utf8len) / 2 ;
-		  pad_right = width - utf8len - pad_left;
-		  break;
-	  case right:
-		  pad_right = 1;
-		  pad_left = width - utf8len - pad_right;
-		  break;
-	  case right_nopad:
-		  pad_right = 0;
-		  pad_left = width - utf8len - pad_right;
-		  break;
+  case left:
+    pad_left = 1;
+    pad_right = width - utf8len - pad_left;
+    break;
+  case left_nopad:
+    pad_left = 0;
+    pad_right = width - utf8len - pad_left;
+    break;
+  case center:
+    pad_left = (width - utf8len) / 2;
+    pad_right = width - utf8len - pad_left;
+    break;
+  case right:
+    pad_right = 1;
+    pad_left = width - utf8len - pad_right;
+    break;
+  case right_nopad:
+    pad_right = 0;
+    pad_left = width - utf8len - pad_right;
+    break;
   }
 
   pad(pad_left, ' ', stream);
-  fprintf(stream, ""SV_FMT"", SV_ARG(s));
+  fprintf(stream, "" SV_FMT "", SV_ARG(s));
   pad(pad_right, ' ', stream);
 }
 
@@ -147,7 +151,7 @@ string_view slurp_stream(FILE *stream) {
   return ret;
 }
 
-geometry get_table_geometry(string_view f, char delim) {
+geometry get_table_geometry(string_view f) {
   geometry ret = {0};
 
   while (f.len > 0) {
@@ -156,7 +160,7 @@ geometry get_table_geometry(string_view f, char delim) {
     size_t c = 0;
 
     while (line.len > 0) {
-      sv_split(&line, delim);
+      sv_split(&line, *delim);
       c++; // ignore the part following the last delimiter
     }
     if (ret.cols < c)
@@ -167,15 +171,15 @@ geometry get_table_geometry(string_view f, char delim) {
 }
 
 void read_table(geometry g, string_view f, string_view *table, size_t *width,
-                char *separators, char delim) {
+                char *separators) {
   size_t l = 0, c;
   while (f.len > 0) {
     string_view line = sv_split(&f, '\n');
     separators[l] = separator_line(line);
     c = 0;
     while (line.len > 0) {
-      table[l * g.cols + c] = sv_trim(sv_split(&line, delim));
-	  size_t utf8len = sv_len_utf_8(table[l * g.cols + c]);
+      table[l * g.cols + c] = sv_trim(sv_split(&line, *delim));
+      size_t utf8len = sv_len_utf_8(table[l * g.cols + c]);
       if (width[c] < utf8len && !separators[l])
         width[c] = utf8len;
       c++;
@@ -185,21 +189,23 @@ void read_table(geometry g, string_view f, string_view *table, size_t *width,
 }
 
 void print_table(geometry g, string_view *table, size_t *width,
-                 char *separators, char delim, FILE *stream) {
+                 char *separators, FILE *stream) {
   for (size_t i = 0; i < g.lines; i++) {
     if (separators[i]) {
-      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1 , stream, left_nopad);
-      fprintf(stream, "%c", delim);
+      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1, stream,
+                  left_nopad);
+      fprintf(stream, "%c", *delim);
       for (size_t j = 1; j < g.cols; j++) {
         pad(width[j] + 2, separators[i], stream);
-        printf("|");
+        printf("%c", *delim);
       }
     } else {
-      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1, stream, left_nopad);
-      fprintf(stream, "%c", delim);
+      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1, stream,
+                  left_nopad);
+      fprintf(stream, "%c", *delim);
       for (size_t j = 1; j < g.cols; j++) {
         print_entry(table[i * g.cols + j], width[j] + 2, stream, center);
-        fprintf(stream, "%c", delim);
+        fprintf(stream, "%c", *delim);
       }
     }
     printf("\n");
