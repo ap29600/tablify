cc9ff85cffad8ed9e47e4203b4dcfe0bc809bca5
Andrea Piseri <andrea.piseri@gmail.com>
2021-08-12 11:28:28 +0200

    Merge `get_table_geometry` and `read_table`
    Should be better for maintainability and consistency
    between estimated size and read elements.

--- tablify.c
+++ tablify.c
@@ -10,7 +10,7 @@ char *input = "";
 typedef struct {
   size_t lines;
   size_t cols;
-} geometry;
+} Geo;
 
 typedef enum {
   center = 0,
@@ -18,17 +18,19 @@ typedef enum {
   left_nopad,
   right,
   right_nopad,
-} align;
+} Align;
+
+typedef string_view Sv;
 
 int contains(char *s, char c);
-int sv_len_utf_8(string_view s);
-char separator_line(string_view s);
+int sv_len_utf_8(Sv s);
+char separator_line(Sv s);
 void pad(size_t width, char fill, FILE *stream);
-void print_entry(string_view s, size_t width, FILE *stream, align align);
-string_view slurp_stream(FILE *stream);
-geometry get_table_geometry(string_view f);
-void read_table(geometry g, string_view f, string_view *table, size_t *width, char *separators);
-void print_table(geometry g, string_view *table, size_t *width, char *separators, FILE *stream);
+void print_entry(Sv s, size_t width, FILE *stream, Align align);
+Sv slurp_stream(FILE *stream);
+Geo read_table(Geo g, Sv f, Sv *table, size_t *width, char *separators);
+void print_table(Geo g, Sv *table, size_t *width, char *separators,
+                 FILE *stream);
 
 int main(int argc, char **argv) {
   arg_string("--ignore", &ignore,
@@ -39,21 +41,21 @@ int main(int argc, char **argv) {
   arg_string("--input", &input, "the input file");
   arg_parse(argc, argv);
 
-  FILE * stream = stdin;
+  FILE *stream = stdin;
   if (strlen(input))
-      stream = fopen(input, "r");
+    stream = fopen(input, "r");
 
-  string_view f = slurp_stream(stream);
+  Sv f = slurp_stream(stream);
 
   // read the table once to get the dimensions
-  geometry g = get_table_geometry(f);
+  Geo g = read_table((Geo){0}, f, 0, 0, 0);
 
   // allocate a table for the SV's, an array for width info, and one for
   // the separator positions.
-  string_view *table = calloc(g.lines * g.cols * sizeof(string_view) + // table
-                                  g.cols * sizeof(size_t) +            // widths
-                                  g.lines * sizeof(char), // separators
-                              1);
+  Sv *table = calloc(g.lines * g.cols * sizeof(Sv) + // table
+                         g.cols * sizeof(size_t) +   // widths
+                         g.lines * sizeof(char),     // separators
+                     1);
   size_t *width = (size_t *)(table + g.lines * g.cols);
   char *separators = (char *)(width + g.cols);
 
@@ -72,7 +74,7 @@ int contains(char *s, char c) {
   return 0;
 }
 
-int sv_len_utf_8(string_view s) {
+int sv_len_utf_8(Sv s) {
   int ret = 0;
   for (; s.len > 0; s.len--)
     if (((*s.data++) & 0xc0) != 0x80)
@@ -82,13 +84,13 @@ int sv_len_utf_8(string_view s) {
 
 // return the separator character if the line is supposed to be a
 // separator line, otherwise return 0.
-char separator_line(string_view s) {
+char separator_line(Sv s) {
   char sep = '\0';
   for (size_t i = 0; i < s.len; i++)
     if (!sep && contains(seps, s.data[i]))
       sep = s.data[i];
-    else if (isspace(s.data[i]) || s.data[i] == sep ||
-             s.data[i] == *delim || contains(ignore, s.data[i]))
+    else if (isspace(s.data[i]) || s.data[i] == sep || s.data[i] == *delim ||
+             contains(ignore, s.data[i]))
       continue;
     else
       return 0;
@@ -101,7 +103,7 @@ void pad(size_t width, char fill, FILE *stream) {
   }
 }
 
-void print_entry(string_view s, size_t width, FILE *stream, align align) {
+void print_entry(Sv s, size_t width, FILE *stream, Align align) {
   size_t pad_left = 0, pad_right = 0, utf8len = sv_len_utf_8(s);
 
   switch (align) {
@@ -132,79 +134,66 @@ void print_entry(string_view s, size_t width, FILE *stream, align align) {
   pad(pad_right, ' ', stream);
 }
 
-string_view slurp_stream(FILE *stream) {
-  string_view ret = {0};
+Sv slurp_stream(FILE *stream) {
+  Sv ret = {0};
   size_t capacity = 0;
   char c;
-
   while (EOF != (c = getc(stream))) {
     if (capacity < ret.len + 1) {
       capacity = (capacity == 0) ? 1024 : capacity * 2;
       ret.data = realloc(ret.data, capacity);
       if (!ret.data)
-        return (string_view){0};
+        return (Sv){0};
     }
-
     ret.data[ret.len++] = c;
   }
   return ret;
 }
 
-geometry get_table_geometry(string_view f) {
-  geometry ret = {0};
-
-  while (f.len > 0) {
-    string_view line = sv_split(&f, '\n');
-    ret.lines++;
-    size_t c = 0;
-    while (line.len > 0) { 
-      sv_split(&line, *delim);
-      line = sv_trim(line);
-      c++; // ignore the part following the last delimiter
-    }
-    if (ret.cols < c)
-      ret.cols = c;
-  }
-
-  return ret;
-}
-
-void read_table(geometry g, string_view f, string_view *table, size_t *width,
-                char *separators) {
+Geo read_table(Geo g, Sv f, Sv *table, size_t *widths, char *sep) {
+  Geo ret = {0};
   size_t l = 0, c;
+
   while (f.len > 0) {
-    string_view line = sv_split(&f, '\n');
-    separators[l] = separator_line(line);
+    Sv line = sv_split(&f, '\n');
+    ret.lines++;
+    if (sep)
+      sep[l] = separator_line(line);
     c = 0;
     while (line.len > 0) {
-      table[l * g.cols + c] = sv_trim(sv_split(&line, *delim));
-      size_t utf8len = sv_len_utf_8(table[l * g.cols + c]);
-      if (width[c] < utf8len && !separators[l])
-        width[c] = utf8len;
+      Sv entry = sv_trim(sv_split(&line, *delim));
+      if (table && widths && sep) {
+        table[l * g.cols + c] = entry;
+        size_t utf8len = sv_len_utf_8(entry);
+        if (widths[c] < utf8len && !sep[l])
+          widths[c] = utf8len;
+      }
       line = sv_trim(line);
       c++;
     }
+    if (ret.cols < c)
+      ret.cols = c;
     l++;
   }
+  return ret;
 }
 
-void print_table(geometry g, string_view *table, size_t *width,
-                 char *separators, FILE *stream) {
+void print_table(Geo g, Sv *table, size_t *widths, char *sep, FILE *stream) {
   for (size_t i = 0; i < g.lines; i++) {
-    if (separators[i]) {
-      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1, stream,
+    if (sep[i]) {
+      print_entry(table[i * g.cols], widths[0] == 0 ? 0 : widths[0] + 1, stream,
                   left_nopad);
       fprintf(stream, "%c", *delim);
       for (size_t j = 1; j < g.cols; j++) {
-        pad(width[j] + 2, separators[i], stream);
+        pad(widths[j] + 2, sep[i], stream);
         printf("%c", *delim);
       }
     } else {
-      print_entry(table[i * g.cols], width[0] == 0 ? 0 : width[0] + 1, stream,
+      print_entry(table[i * g.cols], widths[0] == 0 ? 0 : widths[0] + 1, stream,
                   left_nopad);
       fprintf(stream, "%c", *delim);
       for (size_t j = 1; j < g.cols; j++) {
-        print_entry(table[i * g.cols + j], width[j] + 2, stream, center);
+        print_entry(table[i * g.cols + j], widths[j] + 2, stream, center);
         fprintf(stream, "%c", *delim);
       }
     }
